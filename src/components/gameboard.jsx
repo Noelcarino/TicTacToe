caf6323769058eceabc47a13fc0fc87b983d7bd9
Noel Carino <leononirac1992@gmail.com>
2020-01-30 17:23:26 -0800

    1/30 Succesfully got game condition to work for horizontal and vertical for x

--- src/components/gameboard.jsx
+++ src/components/gameboard.jsx
@@ -14,6 +14,58 @@ export default class GameBoard extends React.Component {
             player2turn: false
         }
         this.handleSquarePieceChange = this.handleSquarePieceChange.bind(this);
+        this.handleGameCondition = this.handleGameCondition.bind(this);
+    }
+    handleGameReset(){
+        let resetGameBoard = [
+            ['','',''],
+            ['','',''],
+            ['','',''],
+        ];
+        let classArray = [
+            'top-left',
+            'top-middle',
+            'top-right',
+            'middle-left',
+            'middle-middle',
+            'middle-right',
+            'bottom-left',
+            'bottom-middle',
+            'bottom-right'
+        ]
+        this.setState({gameBoard: resetGameBoard, player1turn: true, player2turn: false});
+
+        for(var i = 0; i < 9; i++){
+            let element = document.getElementById(classArray[i]);
+            element.classList.remove('lol');
+            element.classList.remove('lol2');
+        }
+    }
+    handleGameCondition(){
+        /*
+            Case 1: x wins
+                x must have length of 3 next to each other
+            Case 2: o wins
+            Caes 3: all tiles filled and case 1 or two 
+                default case = draw;
+        */
+
+        // check horizontal && vertical case;
+
+        // first row check;
+        let row1x = 0;
+        let col1x = 0;
+        for (var i = 0; i < 3; i++){
+            if (this.state.gameBoard[0][i] === 'x') row1x++;
+            if (this.state.gameBoard[i][0] === 'x') col1x++;
+        }
+
+        if (row1x === 3 || col1x === 3) {
+            alert ("x is the winner")
+            row1x = 0;
+            col1x = 0;
+            this.handleGameReset();
+        };
     }
     handleSquarePieceChange(event){
 
@@ -78,7 +130,7 @@ export default class GameBoard extends React.Component {
                 console.log("hello");
                 break;
         }
-        console.log(this.state.gameBoard);
+        this.handleGameCondition();
     }
     handleClassSwitch(param){
 
